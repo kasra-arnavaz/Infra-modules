b7e270ac8ebcc3581160df47ca6912cbc3abe960
kasra-l1r <kasra@di.ku.dk>
2024-03-11 00:21:55 +0100

    Fixed docker image not working on localhost

--- examples/docker-image/build.sh
+++ examples/docker-image/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-docker_path="../../images/docker"
+script_dir="$(dirname "$0")"
+docker_path="${script_dir}/../../images/docker"
 image_name="flask-app"
 container_name="${image_name}-container"
 tag_name="kasraz/${image_name}"
 
-container_exists = $(docker ps -a -q -f name=$container_name)
 docker build -t $image_name $docker_path
 docker tag $image_name $tag_name
 docker push $tag_name
-if [ -n "$container_exists"]; then
-    docker rm $image_name-container
+# Remove container if it already exists
+if [ $(docker ps -aq -f name=^/${container_name}$) ]; then
+    docker stop $container_name
+    docker rm $container_name
 fi
 docker run --name $container_name -p 5000:5000 -d $tag_name
