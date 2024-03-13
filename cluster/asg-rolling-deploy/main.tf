92e4eaac33e24bac533953f2f20bc42b96a3e9ad
kasra-l1r <kasra@di.ku.dk>
2024-03-13 23:23:35 +0100

    Added allow outbound sg for the cluster

--- cluster/asg-rolling-deploy/main.tf
+++ cluster/asg-rolling-deploy/main.tf
@@ -56,14 +56,14 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   cidr_blocks       = local.all_ips
 }
 
-# resource "aws_security_group_rule" "allow_all_outbound" {
-#   type              = "egress"
-#   security_group_id = aws_security_group.instance.id
-#   from_port         = local.any_port
-#   to_port           = local.any_port
-#   protocol          = local.any_protocol
-#   cidr_blocks       = local.all_ips
-# }
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.instance.id
+  from_port         = local.any_port
+  to_port           = local.any_port
+  protocol          = local.any_protocol
+  cidr_blocks       = local.all_ips
+}
 
 locals {
   any_port     = 0
